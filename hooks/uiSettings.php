e3ba047668e8f64a1ff394b49c15bcdc6db7896e
Andrew Rivett <veggiefrog@gmail.com>
2016-09-10 17:41:46 -0400

    Added hooks/uiSettings for new business menu

---
+++ hooks/uiSettings.php
@@ -0,0 +1,82 @@
+<?php
+//
+// Description
+// -----------
+// This function will return a list of user interface settings for the module.
+//
+// Arguments
+// ---------
+// ciniki:
+// business_id:     The ID of the business to get events for.
+//
+// Returns
+// -------
+//
+function ciniki_wineproduction_hooks_uiSettings($ciniki, $business_id, $args) {
+
+    //
+    // Setup the default response
+    //
+    $rsp = array('stat'=>'ok', 'menu_items'=>array());
+
+    //
+    // Check if wineproduction flag is set, and if the user has permissions
+    //
+    if( isset($ciniki['business']['modules']['ciniki.wineproduction'])
+        && (isset($args['permissions']['owners'])
+            || isset($args['permissions']['employees'])
+            || isset($args['permissions']['resellers'])
+            || ($ciniki['session']['user']['perms']&0x01) == 0x01
+            )
+        ) {
+        $menu_item = array(
+            'priority'=>6300,
+            'label'=>'Wine Production', 
+            'edit'=>array('app'=>'ciniki.wineproduction.main'),
+            'add'=>array('app'=>'ciniki.wineproduction.main', 'args'=>array('add'=>'\'"yes"\'')),
+            'search'=>array(
+                'method'=>'ciniki.wineproduction.searchQuick',
+                'args'=>array(),
+                'container'=>'orders',
+                'cols'=>8,
+                'headerValues'=>array('INV#', 'Wine', 'Type', 'BD', 'OD', 'SD', 'RD', 'FD'),
+                'cellClasses'=>array('multiline', 'multiline', 'multiline', 'multiline aligncenter', 'multiline aligncenter', 'multiline aligncenter', 'multiline aligncenter', 'multiline aligncenter'),
+                'cellValues'=>array(
+                    '0'=>'\'<span class="maintext">\' + d.order.invoice_number + \'</span><span class="subtext">\' + M.ciniki_businesses_main.statusOptions[d.order.status] + \'</span>\'',
+                    '1'=>'\'<span class="maintext">\' + d.order.wine_name + \'</span><span class="subtext">\' + d.order.customer_name + \'</span>\'',
+                    '2'=>'\'<span class="maintext">\' + d.order.wine_type + \'</span><span class="subtext">\' + d.order.kit_length + \'&nbsp;weeks</span>\'',
+                    '3'=>'if( d.order.bottling_date != null && d.order.bottling_date != \'\' ) { d.order.bottling_date.replace(/(...)\s([0-9]+),\s([0-9][0-9][0-9][0-9])/, \'<span class="maintext">$1</span><span class="subtext">$2</span>\') } else { \'\'; }',
+                    '4'=>'if( d.order.order_date != null && d.order.order_date != \'\' ) { d.order.order_date.replace(/(...)\s([0-9]+),\s([0-9][0-9][0-9][0-9])/, \'<span class="maintext">$1</span><span class="subtext">$2</span>\') } else { \'\'; }',
+                    '5'=>'if( d.order.start_date != null && d.order.start_date != \'\' ) { d.order.start_date.replace(/(...)\s([0-9]+),\s([0-9][0-9][0-9][0-9])/, \'<span class="maintext">$1</span><span class="subtext">$2</span>\') } else { \'\'; }',
+                    '6'=>'if( d.order.racking_date != null && d.order.racking_date != \'\' ) { d.order.racking_date.replace(/(...)\s([0-9]+),\s([0-9][0-9][0-9][0-9])/, \'<span class="maintext">$1</span><span class="subtext">$2</span>\') } else { \'\'; }',
+                    '7'=>'if( d.order.filtering_date != null && d.order.filtering_date != \'\' ) { d.order.filtering_date.replace(/(...)\s([0-9]+),\s([0-9][0-9][0-9][0-9])/, \'<span class="maintext">$1</span><span class="subtext">$2</span>\') } else { \'\'; }',
+                    ),
+                'noData'=>'No wineproduction found',
+                'edit'=>array('method'=>'ciniki.wineproduction.main', 'args'=>array('order_id'=>'d.order.id;')),
+                'submit'=>array('method'=>'ciniki.wineproduction.main', 'args'=>array('search'=>'search_str')),
+                ),
+            );
+        $rsp['menu_items'][] = $menu_item;
+    } 
+
+    //
+    // Check permissions for what menu items should be available
+    //
+    if( isset($ciniki['business']['modules']['ciniki.wineproduction'])
+        && (isset($args['permissions']['owners'])
+            || isset($args['permissions']['employees'])
+            || isset($args['permissions']['resellers'])
+            || ($ciniki['session']['user']['perms']&0x01) == 0x01
+            )
+        ) {
+        $menu_item = array(
+            'priority'=>3900,
+            'label'=>'Production Schedule', 
+            'edit'=>array('app'=>'ciniki.wineproduction.main', 'args'=>array('schedule'=>'"\'today\'"')),
+            );
+        $rsp['menu_items'][] = $menu_item;
+    } 
+
+    return $rsp;
+}
+?>
