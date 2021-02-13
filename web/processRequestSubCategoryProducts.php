ec812e32ea1b2e8f97b516db99d23998556f108c
Andrew Rivett <veggiefrog@gmail.com>
2021-02-12 20:51:35 -0500

    Added oak, body, dryness as subtitle to image list for products

--- web/processRequestSubCategoryProducts.php
+++ web/processRequestSubCategoryProducts.php
@@ -14,32 +14,33 @@
 //
 function ciniki_wineproduction_web_processRequestSubCategoryProducts(&$ciniki, $settings, $tnid, $category, $subcategory) {
 
-    $strsql = "SELECT ciniki_wineproduction_products.id, "
-        . "ciniki_wineproduction_products.name AS title, "
-        . "ciniki_wineproduction_products.ptype, "
-        . "ciniki_wineproduction_products.permalink, "
-        . "ciniki_wineproduction_products.primary_image_id AS image_id, "
-        . "ciniki_wineproduction_products.unit_amount, "
-        . "ciniki_wineproduction_products.unit_discount_amount, "
-        . "ciniki_wineproduction_products.unit_discount_percentage, "
-        . "ciniki_wineproduction_products.taxtype_id, "
-        . "ciniki_wineproduction_products.inventory_current_num, "
-        . "ciniki_wineproduction_products.flags, "
-        . "ciniki_wineproduction_products.synopsis AS description, "
-        . "'yes' AS is_details "
+    $strsql = "SELECT products.id, "
+        . "products.name AS title, "
+        . "products.ptype, "
+        . "products.permalink, "
+        . "products.primary_image_id AS image_id, "
+        . "products.unit_amount, "
+        . "products.unit_discount_amount, "
+        . "products.unit_discount_percentage, "
+        . "products.taxtype_id, "
+        . "products.inventory_current_num, "
+        . "products.flags, "
+        . "products.synopsis AS description, "
+        . "'yes' AS is_details, "
+        . "IFNULL(tags.tag_name, '') AS subtitle "
         . "FROM ciniki_wineproduction_product_tags AS t1 "
-        . "INNER JOIN ciniki_wineproduction_products ON ("
-            . "t1.product_id = ciniki_wineproduction_products.id "
-            . "AND ciniki_wineproduction_products.tnid = '" . ciniki_core_dbQuote($ciniki, $tnid) . "' "
-            . "AND ciniki_wineproduction_products.start_date < UTC_TIMESTAMP() "
-            . "AND (ciniki_wineproduction_products.end_date = '0000-00-00 00:00:00' "
-                . "OR ciniki_wineproduction_products.end_date > UTC_TIMESTAMP()"
+        . "INNER JOIN ciniki_wineproduction_products AS products ON ("
+            . "t1.product_id = products.id "
+            . "AND products.tnid = '" . ciniki_core_dbQuote($ciniki, $tnid) . "' "
+            . "AND products.start_date < UTC_TIMESTAMP() "
+            . "AND (products.end_date = '0000-00-00 00:00:00' "
+                . "OR products.end_date > UTC_TIMESTAMP()"
                 . ") "
-            . "AND ciniki_wineproduction_products.status < 60 "
-            . "AND (ciniki_wineproduction_products.flags&0x01) > 0 "
+            . "AND products.status < 60 "
+            . "AND (products.flags&0x01) > 0 "
             . ") "
         . "INNER JOIN ciniki_wineproduction_product_tags AS t2 ON ("
-            . "ciniki_wineproduction_products.id = t2.product_id "
+            . "products.id = t2.product_id "
             . "AND t2.permalink = '" . ciniki_core_dbQuote($ciniki, $subcategory['permalink']) . "' "
             . "AND t2.tnid = '" . ciniki_core_dbQuote($ciniki, $tnid) . "' ";
 //    if( isset($category['tag_type']) && $category['tag_type'] > 0 ) {
@@ -48,16 +49,23 @@ function ciniki_wineproduction_web_processRequestSubCategoryProducts(&$ciniki, $
         $strsql .= "AND t2.tag_type > 10 AND t2.tag_type < 30 ";
 //    }
     $strsql .= ") "
+        . "LEFT JOIN ciniki_wineproduction_product_tags AS tags on ("
+            . "products.id = tags.product_id "
+            . "AND tags.tag_type IN (13,14,15) "
+            . "AND tags.tnid = '" . ciniki_core_dbQuote($ciniki, $tnid) . "' "
+            . ") "
         . "WHERE t1.tnid = '" . ciniki_core_dbQuote($ciniki, $tnid) . "' "
         . "AND t1.tag_type = 10 "
         . "AND t1.permalink = '" . ciniki_core_dbQuote($ciniki, $category['permalink']) . "' "
-        . "ORDER BY ciniki_wineproduction_products.name COLLATE latin1_general_cs ASC "
+        . "ORDER BY products.name COLLATE latin1_general_cs ASC, tags.tag_type, tags.tag_name "
         . "";
     $rc = ciniki_core_dbHashQueryIDTree($ciniki, $strsql, 'ciniki.products', array(
         array('container'=>'products', 'fname'=>'id', 
             'fields'=>array('id', 'ptype', 'title', 'permalink', 'image_id', 'description', 'is_details',
                 'unit_amount', 'unit_discount_amount', 'unit_discount_percentage', 'taxtype_id', 'inventory_current_num', 'flags',
-            )),
+                'subtitle'),
+            'dlists'=>array('subtitle'=>', '),
+            ),
         ));
     if( $rc['stat'] != 'ok' ) {
         return $rc;
@@ -67,6 +75,8 @@ function ciniki_wineproduction_web_processRequestSubCategoryProducts(&$ciniki, $
     } else {
         $products = array();
     }
+        error_log(print_r($products,true));
+    
 
     return array('stat'=>'ok', 'products'=>$products);
 }
